ce8872435ace7ff3070f7a65bf2d4896a4d229dc
Jansen Cruz <103548207+jansencruz23@users.noreply.github.com>
2023-02-11 22:02:22 +0800

    Add alert rock paper scissors

---
+++ main.js
@@ -0,0 +1,36 @@
+let ctr = 1;
+do{
+    let player = prompt("Rock, Paper or Scissors? ").toLowerCase();
+    let bot = getComputerChoice();
+    
+    alert(play(player, bot));
+    ctr++;
+}
+while(ctr <= 5);
+
+
+function getComputerChoice(){
+    let choice = Math.ceil(Math.random() * 3);
+    if(choice == 1) return 'rock';
+    else if(choice == 2) return 'paper';
+    else return 'scissors';
+}
+
+function play(player, bot){
+    if((player == 'rock' && bot == 'rock') || (player == 'paper' && bot == 'paper') || (player == 'scissors' && bot == 'scissors'))
+    {
+        return `Draw! you both picked ${player}`;
+    }
+    else if((player == 'rock' && bot == 'scissors') || (player == 'paper' && bot == 'rock') || (player == 'scissors' && bot == 'paper'))
+    {
+        return `You Win! ${player} beats ${bot}`;
+    }
+    else if((player == 'rock' && bot == 'paper') || (player == 'paper' && bot == 'scissors') || (player == 'scissors' && bot == 'rock'))
+    {
+        return `You Lose! ${bot} beats ${player}`;
+    }
+    else
+    {
+        return 'Invalid';
+    }
+}
