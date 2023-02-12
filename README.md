9cd76afef6189500a96dbcb31aa174df8f647dbd
Jansen Cruz <103548207+jansencruz23@users.noreply.github.com>
2023-02-12 15:55:27 +0800

    Fix image position, add scoring, and winner using modal but not yet fully functional

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Rock-Paper-Scissors-Web
 Rock Paper Scissors using Vanilla JS
+
+This is a beginner project made by Jansen C. Cruz
+This website is inspired by 'michalosman' on GitHub
--- main.js
+++ main.js
@@ -1,14 +1,45 @@
-const picks = document.querySelectorAll(".picks");
+const picks = document.querySelectorAll(".pick");
+const userIcon = document.querySelector("#user-icon");
+const botIcon = document.querySelector("#bot-icon");
+const header = document.querySelector(".header h1");
+const subHeader = document.querySelector(".header p");
+const userScoreDiv = document.querySelector("#user-score");
+const botScoreDiv = document.querySelector("#bot-score");
+const modal = document.querySelector(".modal-box");
+const MAX_SCORE = 5;
+
+let userScore = 0;
+let botScore = 0;
 
 picks.forEach((pick) => {
     pick.addEventListener('click', (e) => {
         let userPick = getUserChoice(e);
         let botPick = getComputerChoice();
-    
+
+        displayPick(userPick, botPick);
         play(userPick, botPick);
+
+        if(userScore >= MAX_SCORE || botScore >= MAX_SCORE){
+            modal.style.display = 'block';
+        }
     });
 });
 
+function displayPick(userPick, botPick){
+    switch(userPick)
+    {
+        case 'rock': userIcon.textContent = '✊'; break;
+        case 'paper': userIcon.textContent = '🖐'; break;
+        case 'scissors': userIcon.textContent = '✌️'; break;
+    }
+
+    switch(botPick)
+    {
+        case 'rock': botIcon.textContent = '✊'; break;
+        case 'paper': botIcon.textContent = '🖐'; break;
+        case 'scissors': botIcon.textContent = '✌️'; break;
+    }
+}
 
 function getUserChoice(e){
     if(e.target.id == 'rock') return 'rock';
@@ -26,15 +57,22 @@ function getComputerChoice(){
 function play(player, bot){
     if((player == 'rock' && bot == 'rock') || (player == 'paper' && bot == 'paper') || (player == 'scissors' && bot == 'scissors'))
     {
-        alert( `Draw! you both picked ${player}`);
+        header.textContent = "It's a tie!";
+        subHeader.textContent = `${player} ties with ${bot}`;
     }
     else if((player == 'rock' && bot == 'scissors') || (player == 'paper' && bot == 'rock') || (player == 'scissors' && bot == 'paper'))
     {
-        alert( `You Win! ${player} beats ${bot}`);
+        header.textContent = "You win!";
+        subHeader.textContent = `${player} beats ${bot}`;
+        userScore++;
+        userScoreDiv.innerHTML = `Player: ${userScore}`;
     }
     else if((player == 'rock' && bot == 'paper') || (player == 'paper' && bot == 'scissors') || (player == 'scissors' && bot == 'rock'))
     {
-        alert( `You Lose! ${bot} beats ${player}`);
+        header.textContent = "You lost!";
+        subHeader.textContent = `${player} is beaten by ${bot}`;
+        botScore++;
+        botScoreDiv.innerHTML = `Player: ${botScore}`;
     }
     else
     {
